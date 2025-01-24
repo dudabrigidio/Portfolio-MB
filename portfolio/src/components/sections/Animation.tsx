f126f86b952fbeb4b5f825959032580250c4f942
dudabrigidio <dudabrigidio@gmail.com>
2025-01-24 00:47:11 -0300

    att

--- portfolio/src/components/sections/Animation.tsx
+++ portfolio/src/components/sections/Animation.tsx
@@ -18,7 +18,7 @@ const Animation: React.FC<AnimationProps> = ({ children }) => {
             
             <span
                 style={{
-                transform: isInView ? "none" : "translate(200px)",
+                transform: isInView ? "none" : "translateX(200px)",
                 opacity: isInView ? 1 : 0,
                 transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s"
                 }}
